cf924d2f3000d8ad226c5da56e6b908e6297a680
lcorelli <leonardcorelli@gmail.com>
2022-04-26 21:09:56 -0400

    can now show representative tweets of each topic cluster

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from scripts.lib_functions import get_tweets, convert_to_df, clean_tweets, get_clusters
 import os
+import pandas as pd
 from bertopic import BERTopic
 
 import nltk
@@ -16,7 +17,7 @@ st.image("./images/Twitter-logo.png")
 term = st.text_input("Term you would like to search:")
 
 if term:
-    messy_tweets = get_tweets(term, max_tweets=500) # search term
+    messy_tweets = get_tweets(term, max_tweets=1000) # search term
     tweets = convert_to_df(messy_tweets) # messy tweets
     processed_tweets = clean_tweets(tweets) # stop words removed, lemmatized, removed @'s and web links
     
@@ -26,5 +27,13 @@ if term:
 
     #st.dataframe(processed_tweets)
     clusters = get_clusters(info)
+
+    rep_docs = topic_model.get_representative_docs()
+
+    pairwise_df = pd.concat([tweets["tweet"], processed_tweets["tweet"]],axis=1)
+    pairwise_df.columns = ["original_tweet","cleaned_tweet"]
+
     for i in range(clusters.shape[0]):
         st.markdown("## **"+f"Topic {i} (" + str(clusters["Count"].iloc[i]) + " tweets): " + clusters["clean_clusters"].iloc[i][0] + ", " + clusters["clean_clusters"].iloc[i][1] + ", " + clusters["clean_clusters"].iloc[i][2] + ", " + clusters["clean_clusters"].iloc[i][3] + "**")
+        for j in range(len(rep_docs[i])):
+            st.markdown("- " + pairwise_df[pairwise_df["cleaned_tweet"]==rep_docs[i][j]]["original_tweet"].iloc[0])
