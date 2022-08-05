656f3f8a15005cbdb9639a2ba3ffc7c44e734264
lcorelli <leonardcorelli@gmail.com>
2022-08-05 10:57:40 -0400

    error handling for topic with not enough tweets

--- streamlit.py
+++ streamlit.py
@@ -26,26 +26,30 @@ st.image("./images/Twitter-logo.png")
 
 st.write("Let's find out what people are actually saying about a given topic! This microservice is going to pull the 1500 most recent tweets on a topic of your choice and then use machine learning to cluster them and present back to you the relevant subtopics, as well as representative tweets for each of those subtopics.")
 
-term = st.text_input("Term you would like to search:", placeholder="Ex: 'Economy' or 'JetBlue'")
+term = st.text_input("Term you would like to search (please allow ~45 seconds to run):", placeholder="Ex: 'Economy' or 'JetBlue'")
 
 if term:
     messy_tweets = get_tweets(term, max_tweets=1500) # search term
     tweets = convert_to_df(messy_tweets) # messy tweets
     processed_tweets, tweets = clean_tweets(tweets) # stop words removed, lemmatized, removed @'s and web links
     
-    topics, probs = topic_model.fit_transform(processed_tweets["tweet"]) # do topic modeling on the tweets
-    info = topic_model.get_topic_info()
+    try:
+        topics, probs = topic_model.fit_transform(processed_tweets["tweet"]) # do topic modeling on the tweets
+        info = topic_model.get_topic_info()
 
-    clusters = get_clusters(info)
+        clusters = get_clusters(info)
 
-    rep_docs = topic_model.get_representative_docs()
+        rep_docs = topic_model.get_representative_docs()
 
-    pairwise_df = pd.concat([tweets["tweet"], processed_tweets["tweet"]],axis=1)
-    pairwise_df.columns = ["original_tweet","cleaned_tweet"]
+        pairwise_df = pd.concat([tweets["tweet"], processed_tweets["tweet"]],axis=1)
+        pairwise_df.columns = ["original_tweet","cleaned_tweet"]
 
-    pairwise_df["original_tweet"] = pairwise_df["original_tweet"].replace("\n",' ', regex=True)
+        pairwise_df["original_tweet"] = pairwise_df["original_tweet"].replace("\n",' ', regex=True)
 
-    for i in range(clusters.shape[0]): # this for loop iterates through variable length cluster results and neatly displays them using the streamlit framework
-        st.markdown("## **"+f"Topic {i+1} (" + str(clusters["Count"].iloc[i]) + " tweets): " + clusters["clean_clusters"].iloc[i][0] + ", " + clusters["clean_clusters"].iloc[i][1] + ", " + clusters["clean_clusters"].iloc[i][2] + ", " + clusters["clean_clusters"].iloc[i][3] + "**")
-        for j in range(len(rep_docs[i])):
-            st.markdown("- " + pairwise_df[pairwise_df["cleaned_tweet"]==rep_docs[i][j]]["original_tweet"].iloc[0])
+        for i in range(clusters.shape[0]): # this for loop iterates through variable length cluster results and neatly displays them using the streamlit framework
+            st.markdown("## **"+f"Topic {i+1} (" + str(clusters["Count"].iloc[i]) + " tweets): " + clusters["clean_clusters"].iloc[i][0] + ", " + clusters["clean_clusters"].iloc[i][1] + ", " + clusters["clean_clusters"].iloc[i][2] + ", " + clusters["clean_clusters"].iloc[i][3] + "**")
+            for j in range(len(rep_docs[i])):
+                st.markdown("- " + pairwise_df[pairwise_df["cleaned_tweet"]==rep_docs[i][j]]["original_tweet"].iloc[0])
+
+    except ValueError:
+        st.subheader("Not enough tweets on this topic! Please try a more popular topic with at least 1500 related tweets in the last 7 days :)")
